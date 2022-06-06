823d97bfbbfa6e76ac923c36980a7c526d9b33fb
mybffjustin <juh369@g.harvard.edu>
2022-06-05 22:44:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### justinhoang
---
+++ src/main/java/org/justinhoang/controller/MainController.java
@@ -0,0 +1,36 @@
+package org.justinhoang.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/** Defines a controller to handle HTTP requests */
+@Controller
+public final class MainController
+{
+
+  private static String project;
+  private static final Logger logger = LoggerFactory.getLogger(
+          MainController.class);
+
+  /**
+   * Create an endpoint for the landing page
+   *
+   * @return the index view template
+   */
+  @GetMapping("/")
+  public String index(Model model) {
+
+    // Get Cloud Run environment variables.
+    String revision = System.getenv("K_REVISION") == null ? "???" : System.getenv("K_REVISION");
+    String service = System.getenv("K_SERVICE") == null ? "???" : System.getenv("K_SERVICE");
+
+    // Set variables in html template.
+    model.addAttribute("revision", revision);
+    model.addAttribute("service", service);
+    return "index";
+  }
+
+}
